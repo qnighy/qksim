f45c620eedb5563c4c08dc1cd3b2cda19a968d9e
Masaki Hara <ackie.h.gmai@gmail.com>
2014-11-27 23:29:12 +0900

    fix small bug in native_fpu

--- native_fpu.c
+++ native_fpu.c
@@ -27,7 +27,7 @@ uint32_t native_fmul(uint32_t a, uint32_t b) {
   return reinterp_u(reinterp_f(a) * reinterp_f(b));
 }
 uint32_t native_itof(uint32_t a) { return reinterp_u((float)a); }
-uint32_t native_ftoi(uint32_t a) { return (uint32_t)reinterp_f(a); }
+uint32_t native_ftoi(uint32_t a) { return (int32_t)reinterp_f(a); }
 
 int native_feq(uint32_t a, uint32_t b) {
   return reinterp_f(a) == reinterp_f(b);
